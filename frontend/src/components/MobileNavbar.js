842e84e05931033d25dfe5c316fe4fe1bd05810f
Saga Swahn <saga@Sagas-MacBook-Air.local>
2022-04-05 11:06:55 +0200

    Added more styling for mobile devices

--- frontend/src/components/MobileNavbar.js
+++ frontend/src/components/MobileNavbar.js
@@ -15,46 +15,49 @@ const MobileNavbar = () => {
 
   return (
     <div style={showMenu ? { backgroundColor: "rgba(17, 18, 34, 0.975" } : {}}>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={showMenu ? { backgroundColor: "rgba(17, 18, 34, 0.975" } : {}}
+      >
         <div className={styles.elements}>
-          <div></div>
           <button className={styles.button} onClick={() => toggleMenu()}>
             {showMenu ? <IoClose /> : <HiOutlineMenuAlt3 />}
           </button>
         </div>
-      </div>
-      {showMenu && (
-        <>
-          <div className={styles.menu}>
-            <div className={styles.links}>
-              <Link to="/" onClick={() => setShowMenu(false)}>
-                Home
-              </Link>
-              <Link to="/cases" onClick={() => setShowMenu(false)}>
-                Live cases
-              </Link>
-              <Link to="/projects" onClick={() => setShowMenu(false)}>
-                Projects
-              </Link>
-              <Link to="/about" onClick={() => setShowMenu(false)}>
-                About
-              </Link>
-              <Link to="/contact" onClick={() => setShowMenu(false)}>
-                Contact
-              </Link>
-            </div>
 
-            <div className={styles.icons}>
-              <a href="https://github.com/hejsaga">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a href="https://www.linkedin.com/in/sagaswahn/">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a>
+        {showMenu && (
+          <>
+            <div className={styles.menu}>
+              <div className={styles.links}>
+                <Link to="/" onClick={() => setShowMenu(false)}>
+                  Home
+                </Link>
+                <Link to="/cases" onClick={() => setShowMenu(false)}>
+                  Live cases
+                </Link>
+                <Link to="/projects" onClick={() => setShowMenu(false)}>
+                  Projects
+                </Link>
+                <Link to="/about" onClick={() => setShowMenu(false)}>
+                  About
+                </Link>
+                <Link to="/contact" onClick={() => setShowMenu(false)}>
+                  Contact
+                </Link>
+              </div>
+
+              <div className={styles.icons}>
+                <a href="https://github.com/hejsaga">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://www.linkedin.com/in/sagaswahn/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
